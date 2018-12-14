5d89ce912e5df72919df2c1404c82807395fc1bb
Quentin Bts <bettens.q@gmail.com>
2018-12-14 14:14:27 +0100

    add Car

---
+++ src/Car.java
@@ -0,0 +1,22 @@
+public class Car {
+
+    private int NChassis;
+    private String Nimma;
+
+    public int getNChassis() {
+        return NChassis;
+    }
+
+    public void setNChassis(int NChassis) {
+        this.NChassis = NChassis;
+    }
+
+    public String getNimma() {
+        return Nimma;
+    }
+
+    public void setNimma(String nimma) {
+        Nimma = nimma;
+    }
+
+}
