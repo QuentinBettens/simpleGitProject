f7e9ddbe81f3b67febc331c1b9ad996674cbfeed
Quentin Bts <bettens.q@gmail.com>
2018-12-14 16:37:16 +0100

    MotorBike second commit watch nb of wheels for motorbike

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Cleaned main");
+        System.out.println("motorbike got " + MotorBike.getNbRoues() + " wheels");
     }
 }
--- src/MotorBike.java
+++ src/MotorBike.java
@@ -1,3 +1,7 @@
 public class MotorBike {
     private final static int NB_ROUES = 2;
+
+    public static int getNbRoues() {
+        return NB_ROUES;
+    }
 }
