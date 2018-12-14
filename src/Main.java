6b1f402b751f92341fc07dd43f052617fc30533c
Quentin Bts <bettens.q@gmail.com>
2018-12-14 16:39:28 +0100

    commit main add a line

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Cleaned main");
         System.out.println("motorbike got " + MotorBike.getNbRoues() + " wheels");
+        System.out.println("push main");
     }
 }
