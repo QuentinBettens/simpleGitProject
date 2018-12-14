5946907521846754695fe8454a04f46c07a590ba
Quentin Bts <bettens.q@gmail.com>
2018-12-14 15:32:25 +0100

    from local cars to remote master

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello masterBranch!");
         System.out.println("test");
+        System.out.println("push in remote master from local branchCars");
     }
 }
