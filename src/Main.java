13d700917f66fe224635f4860e59ae802ab90704
Quentin Bts <bettens.q@gmail.com>
2018-12-14 16:26:43 +0100

    clean main

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello masterBranch!");
-        System.out.println("test");
-        System.out.println("push in remote master from local branchCars");
-        System.out.println("push from plane local to remote local");
-        System.out.println("push from branch boats");
+        System.out.println("Cleaned main");
     }
 }
